fd1c7624bc46c50fd93b2d534ece7d3174f66dd2
sauerbraten <willing.alexander@googlemail.com>
2014-12-14 16:43:02 +0100

    fixed IP prefix size choosing; simpler code for IP padding

--- db/ip.go
+++ db/ip.go
@@ -26,12 +26,12 @@ func intToIP(intIp int64) net.IP {
 	return net.IPv4(abcd[0], abcd[1], abcd[2], abcd[3])
 }
 
-// parses all of the following examples into valid IP ranges:
+// parses all of the following examples into valid IP ranges by padding the IP:
 // 123.
 // 184.29.39.193/16
-// 12.304./8
+// 12.304/8
 // 29.43.223./13
-// IP octets must be complete and end with a dot. prefix size is optional, a fitting prefix size will be chosen in the case it's omitted.
+// prefix size is optional, a fitting prefix size will be chosen in the case it's omitted.
 func getSubnet(cidr string) (ipNet *net.IPNet) {
 	parts := strings.Split(cidr, "/")
 
@@ -43,31 +43,31 @@ func getSubnet(cidr string) (ipNet *net.IPNet) {
 		prefixSize, err = strconv.Atoi(parts[1])
 		if err != nil {
 			prefixSize = 0
-		} else if prefixSize < 0 || prefixSize > 31 {
+		} else if prefixSize < 0 || prefixSize > 24 {
 			prefixSize = 0
 		}
+	}
+
+	if !strings.HasSuffix(ipString, ".") && strings.Count(ipString, ".") < 3 {
+		ipString += "."
 	}
 
 	// not the most elegant way, but meh...
 	switch strings.Count(ipString, ".") {
 	case 1:
-		if strings.HasSuffix(ipString, ".") {
-			ipString += "0"
-		}
-		ipString += ".0.0"
+		ipString += "0.0.0"
 
 		if prefixSize == 0 {
 			prefixSize = 8
 		}
+
 	case 2:
-		if strings.HasSuffix(ipString, ".") {
-			ipString += "0"
-		}
-		ipString += ".0"
+		ipString += "0.0"
 
 		if prefixSize == 0 {
 			prefixSize = 16
 		}
+
 	case 3:
 		if strings.HasSuffix(ipString, ".") {
 			ipString += "0"

0b1393cfe4d7a30ed7796ebe27f8735374e4ca81
sauerbraten <willing.alexander@googlemail.com>
2015-01-19 14:53:29 +0100

    results page now shows details about the lookup performed; nicer HTML and CSS (using flex layout)

--- web/web.go
+++ web/web.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"strings"
 	"text/template"
-	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/sauerbraten/chef/db"
@@ -26,7 +24,7 @@ func main() {
 	r.StrictSlash(true)
 
 	r.HandleFunc("/", front)
-	r.HandleFunc("/lookup", lookupSightings)
+	r.HandleFunc("/lookup", lookup)
 	r.HandleFunc("/status", status)
 	r.Handle("/{fn:[a-z]+\\.css}", http.FileServer(http.Dir("css")))
 
@@ -39,60 +37,8 @@ func main() {
 }
 
 func front(resp http.ResponseWriter, req *http.Request) {
-	http.ServeFile(resp, req, "html/front.html")
-}
-
-type Results struct {
-	Query     string
-	Sightings []db.Sighting
-}
-
-func TimestampToString(timestamp int64) string {
-	return time.Unix(timestamp, 0).UTC().Format("2006-01-02 15:04:05 MST")
-}
-
-func lookupSightings(resp http.ResponseWriter, req *http.Request) {
 	logRequest(req)
-
-	nameOrIP := req.FormValue("q")
-
-	sorting := db.ByNameFrequency
-	if req.FormValue("sorting") == "last_seen" {
-		sorting = db.ByLastSeen
-	}
-
-	directLookupForced := false
-	if req.FormValue("direct") == "true" {
-		directLookupForced = true
-	}
-
-	sightings := storage.Lookup(nameOrIP, sorting, directLookupForced)
-
-	if req.FormValue("plain") == "true" {
-		if len(sightings) == 0 {
-			fmt.Fprintln(resp, "nothing found!")
-			return
-		}
-
-		fmt.Fprintf(resp, "%15s   %-15s   %-23s   %15s   %5s   %s\n\n", "PLAYER IP", "PLAYER NAME", "LAST SEEN", "SERVER IP", "PORT", "SERVER DESCRIPTION")
-
-		for _, sighting := range sightings {
-			fmt.Fprintf(resp, "%15s   %-15s   %-23s   %15s   %5d   %s\n", sighting.IP, sighting.Name, time.Unix(sighting.Timestamp, 0).UTC().Format("2006-01-02 15:04:05 MST"), sighting.ServerIP, sighting.ServerPort, sighting.ServerDescription)
-		}
-	} else {
-		results := Results{
-			Query:     nameOrIP,
-			Sightings: sightings,
-		}
-
-		resultsTempl := template.New("results.html")
-		resultsTempl = resultsTempl.Funcs(template.FuncMap{"timestring": TimestampToString})
-		resultsTempl = template.Must(resultsTempl.ParseFiles("html/results.html"))
-		err := resultsTempl.Execute(resp, results)
-		if err != nil {
-			log.Println(err)
-		}
-	}
+	http.ServeFile(resp, req, "html/front.html")
 }
 
 func status(resp http.ResponseWriter, req *http.Request) {

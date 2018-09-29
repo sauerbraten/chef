a8acce0aeb5dfb5f4d00913cd2725c2a1865d317
sauerbraten <willing.alexander@googlemail.com>
2018-09-29 02:33:38 +0200

    web: move lookup handler to the other handlers

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
 	"html/template"
 	"log"
+	"net"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
 
 	"github.com/sauerbraten/chef/internal/db"
+	"github.com/sauerbraten/chef/internal/ips"
 	"github.com/sauerbraten/chef/internal/kidban"
 )
 
@@ -80,6 +84,60 @@ func (s *server) statusPage() http.HandlerFunc {
 		err := tmpl.Execute(resp, status)
 		if err != nil {
 			log.Println(err)
+		}
+	}
+}
+
+func (s *server) lookup() http.HandlerFunc {
+	tmpl, err := template.
+		New("base.tmpl"). // must be the base template (entry point) so templates are associated correctly by ParseFiles()
+		Funcs(template.FuncMap{
+			"timestring": func(timestamp int64) string { return time.Unix(timestamp, 0).UTC().Format("2006-01-02 15:04:05") },
+			"kidbanned":  func(ip string) bool { return s.kidban.IsBanned(net.ParseIP(ip)) },
+		}).
+		Option("missingkey=error").
+		ParseFiles("templates/base.tmpl", "templates/results.tmpl")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return func(resp http.ResponseWriter, req *http.Request) {
+		nameOrIP := req.FormValue("q")
+
+		sorting := db.ByNameFrequency
+		if req.FormValue("sorting") == db.ByLastSeen.Identifier {
+			sorting = db.ByLastSeen
+		}
+
+		directLookupForced := req.FormValue("direct") == "true"
+
+		// (permanently) redirect partial IP queries
+		if ips.IsPartialOrFullCIDR(nameOrIP) {
+			var subnet *net.IPNet
+			subnet = ips.GetSubnet(nameOrIP)
+
+			if nameOrIP != subnet.String() {
+				u, _ := url.ParseRequestURI(req.RequestURI) // it's safe to assume this will not fail
+				params := u.Query()
+				params.Set("q", subnet.String())
+				u.RawQuery = params.Encode()
+				http.Redirect(resp, req, u.String(), http.StatusPermanentRedirect)
+				return
+			}
+		}
+
+		finishedLookup := s.db.Lookup(nameOrIP, sorting, directLookupForced)
+
+		if req.FormValue("format") == "json" {
+			err := json.NewEncoder(resp).Encode(finishedLookup)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			err := tmpl.Execute(resp, finishedLookup)
+			if err != nil {
+				log.Println(err)
+			}
 		}
 	}
 }
